b0ddec5c9de017ab0adae6c786d25522064d48bc
Eden-Tenenbaum <edenten2405@gmail.com>
2025-03-12 16:14:26 +0200

    init commit
    made the skeleton for the minesweep game functions and a bit of css and html.

---
+++ js/main.js
@@ -0,0 +1,47 @@
+'use strict'
+
+function onInit() {
+}
+
+function setLevel(size, mines) {
+}
+
+function buildBoard() {
+}
+
+
+function placeMines(board) {
+}
+
+function setMinesNegsCount(board) {
+}
+function countNeighborMines(board, rowIdx, colIdx) {
+    //UTIL
+}
+
+function renderBoard(board) {
+}
+
+function onCellClicked(elCell, i, j) {
+
+}
+
+function onCellMarked(event, elCell, i, j) {
+}
+
+function expandReveal(board, i, j) {
+}
+
+
+function checkGameOver() {
+}
+
+function gameOver(isWin) {
+}
+
+function startTimer() {
+}
+
+function getRandomInt(min, max) {
+    //UTILS
+}
